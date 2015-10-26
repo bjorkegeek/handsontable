c983374f503ebb9bf882115eb89d10e4b368ecf4
David Björkevik <david@bjorkevik.se>
2015-10-26 11:53:27 +0100

    Added fixed rows bottom

--- src/helpers/dom/element.js
+++ src/helpers/dom/element.js
@@ -790,35 +790,57 @@ export function setCaretPosition(element, pos, endPos) {
 }
 
 var cachedScrollbarWidth;
+var cachedScrollbarHeight;
 
 //http://stackoverflow.com/questions/986937/how-can-i-get-the-browsers-scrollbar-sizes
 function walkontableCalculateScrollbarWidth() {
   var inner = document.createElement('p');
-  inner.style.width = '100%';
-  inner.style.height = '200px';
+  inner.style.width = "100%";
+  inner.style.height = "200px";
 
   var outer = document.createElement('div');
-  outer.style.position = 'absolute';
-  outer.style.top = '0px';
-  outer.style.left = '0px';
-  outer.style.visibility = 'hidden';
-  outer.style.width = '200px';
-  outer.style.height = '150px';
-  outer.style.overflow = 'hidden';
-  outer.appendChild(inner);
+  outer.style.position = "absolute";
+  outer.style.top = "0px";
+  outer.style.left = "0px";
+  outer.style.visibility = "hidden";
+  outer.style.width = "200px";
+  outer.style.height = "150px";
+  outer.style.overflow = "hidden";
+  outer.appendChild (inner);
 
-  (document.body || document.documentElement).appendChild(outer);
+  document.body.appendChild (outer);
   var w1 = inner.offsetWidth;
   outer.style.overflow = 'scroll';
   var w2 = inner.offsetWidth;
-  if (w1 == w2) {
-    w2 = outer.clientWidth;
-  }
+  if (w1 == w2) w2 = outer.clientWidth;
 
-  (document.body || document.documentElement).removeChild(outer);
+  document.body.removeChild (outer);
 
   return (w1 - w2);
-}
+};
+
+function walkontableCalculateScrollbarHeight() {
+  var inner = document.createElement('div');
+  inner.style.width = "200px";
+  inner.style.height = "200px";
+
+  var outer = document.createElement('div');
+  outer.style.position = "absolute";
+  outer.style.top = "0px";
+  outer.style.left = "0px";
+  outer.style.visibility = "hidden";
+  outer.style.width = "150px";
+  outer.style.height = "150px";
+  outer.style.overflow = 'scroll';
+  outer.appendChild (inner);
+
+  document.body.appendChild (outer);
+  var w1 = outer.offsetHeight;
+  var w2 = outer.clientHeight;
+  document.body.removeChild (outer);
+
+  return (w1 - w2);
+};
 
 /**
  * Returns the computed width of the native browser scroll bar.
@@ -831,6 +853,19 @@ export function getScrollbarWidth() {
   }
 
   return cachedScrollbarWidth;
+}
+
+/**
+ * Returns the computed height of the native browser scroll bar.
+ *
+ * @return {Number} height
+ */
+export function getScrollbarHeight() {
+  if (cachedScrollbarHeight === void 0) {
+    cachedScrollbarHeight = walkontableCalculateScrollbarHeight();
+  }
+
+  return cachedScrollbarHeight;
 }
 
 /**
